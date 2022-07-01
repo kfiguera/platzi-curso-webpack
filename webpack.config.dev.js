94c30b6305197c213e719cfba3493c162f5109ce
Kevin Figuera <kevinalejandro3@gmail.com>
2022-07-01 19:07:25 -0400

    Webpack Bundle Analyzer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 module.exports = {
     entry: './src/index.js',
@@ -68,6 +69,7 @@ module.exports = {
             ]
         }),
         new Dotenv(),
+        new BundleAnalyzerPlugin()
     ],
     devServer: {
         static: path.join(__dirname, 'dist'),
